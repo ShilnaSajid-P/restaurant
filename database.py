563f4f3be45ce0cab9de5a6fcd5a52a6e58a2422
ShilnaSajid-P <shilna@trigeminal.ai.com>
2023-07-15 18:20:34 +0530

    added apis

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date, DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+
 
 engine = create_engine("sqlite:///database.db")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -13,4 +15,15 @@ class User(Base):
     password = Column(String)
     email = Column(String)
     
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+
+class OrderedItem(Base):
+    __tablename__ = "ordered_items"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    item_name = Column(String)
+    quantity = Column(Integer)
+   
 Base.metadata.create_all(bind=engine)
--- routers.py
+++ routers.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Depends, Form, HTTPException, Request
 from fastapi.templating import Jinja2Templates
-from database import SessionLocal, User
+from database import SessionLocal, User,Item,OrderedItem
 from models import UserCreate
 from sqlalchemy.orm import Session
+from typing import List
+
+
 
 
 
@@ -15,13 +18,18 @@ def get_db() -> Session:
         yield db
     finally:
         db.close()
+        
+
 
 @router.post("/user/login")
 async def login(request: Request, username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
     user = db.query(User).filter(User.username == username, User.password == password).first()
     if not user:
         raise HTTPException(status_code=401, detail="Invalid username or password")
-    return templates.TemplateResponse("items.html", {"request": request})
+
+    items = db.query(Item).all()  # Fetch items from the database
+
+    return templates.TemplateResponse("items.html", {"request": request, "items": items})
 
 @router.post("/user/register")
 async def register(user: UserCreate, db = Depends(get_db)):
@@ -42,3 +50,30 @@ async def register(user: UserCreate, db = Depends(get_db)):
     db.commit()
     
     return {"message": "User registered successfully"}
+
+@router.post("/items")
+async def create_item(name: str, db: Session = Depends(get_db)):
+    new_item = Item(name=name)
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+    return new_item
+
+@router.post("/save_items")
+async def save_items(selected_items: List[int] = Form(...), db: Session = Depends(get_db)):
+    items = db.query(Item).filter(Item.id.in_(selected_items)).all()
+
+    ordered_items = []
+    for item in items:
+        ordered_item = OrderedItem(item_name=item.name, quantity=1)  # Set the quantity as needed
+        ordered_items.append(ordered_item)
+
+    db.add_all(ordered_items)
+    db.commit()
+
+    return {"message": "Items saved successfully"}
+
+@router.get("/ordered_items")
+async def get_ordered_items(db: Session = Depends(get_db)):
+    ordered_items = db.query(OrderedItem).all()
+    return ordered_items
